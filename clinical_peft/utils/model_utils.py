6455d2fdb5d3f2445c16dec9409f3da93a14221f
Aryo Pradipta Gema <aryopg@gmail.com>
2023-05-18 11:58:37 +0100

    use roberta to test

--- clinical_peft/utils/model_utils.py
+++ clinical_peft/utils/model_utils.py
@@ -5,7 +5,7 @@ from ..constants import PEFT_CONFIGS, TASK_TYPE
 
 
 def load_peft_config(
-    peft_type: str, task_type: str, peft_hyperparameters
+    peft_type: str, task_type: str, peft_hyperparameters: dict
 ) -> PeftConfig:
     return PEFT_CONFIGS[peft_type](
         task_type=TASK_TYPE[PEFTTaskType[task_type]],
