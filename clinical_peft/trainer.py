7853ef88a492594ad687f8b78a7e6c9cdc9cad4b
Aryo Pradipta Gema <aryopg@gmail.com>
2023-05-18 11:33:39 +0100

    modify to cover biolinkbert

--- clinical_peft/trainer.py
+++ clinical_peft/trainer.py
@@ -290,8 +290,9 @@ def run_sweep(
         dataset = preprocess_dataset(dataset, configs, tokenizer)
     accelerator.wait_for_everyone()
 
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+    if getattr(tokenizer, "pad_token_id") is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
     if configs.model_configs.task_type == PEFTTaskType.causal_lm:
         data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
     elif configs.model_configs.task_type == PEFTTaskType.seq_cls:
