c4681a0a882044460c714b2c22d034fdc31f6477
Aryo Pradipta Gema <aryopg@gmail.com>
2023-07-02 18:47:57 +0100

    debugging

--- clinical_peft/trainer.py
+++ clinical_peft/trainer.py
@@ -243,6 +243,9 @@ def train(
                         )
                     else:
                         outputs = model(**batch, labels=labels)
+                        print("batch['input_ids'].size(): ", batch["input_ids"].size())
+                        print("outputs.size(): ", outputs.size())
+                        print("labels.size(): ", labels.size())
                         loss = outputs.loss
                 else:
                     outputs = model(**batch)
