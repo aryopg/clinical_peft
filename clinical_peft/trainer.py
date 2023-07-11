ad8066b15434af492e225bb0db6bd3ebfaf0a06a
Aryo Pradipta Gema <aryopg@gmail.com>
2023-07-11 21:58:44 +0100

    debugging moving the requires grad

--- clinical_peft/trainer.py
+++ clinical_peft/trainer.py
@@ -202,6 +202,10 @@ def train(
                 is_trainable=configs.model_configs.pretrained_peft_fine_tune,
             )
 
+            for name, param in model.named_parameters():
+                if ".score" in name or ".classifier" in name:
+                    param.requires_grad = True
+
             if configs.model_configs.downstream_peft:
                 pretrained_peft_type = model.peft_config["default"].peft_type
                 downstream_peft_config = PEFT_CONFIGS[pretrained_peft_type.lower()](
@@ -211,10 +215,6 @@ def train(
                 )
 
                 model.add_adapter("lora_downstream", downstream_peft_config)
-
-            for name, param in model.named_parameters():
-                if ".score" in name or ".classifier" in name:
-                    param.requires_grad = True
 
             for name, param in model.named_parameters():
                 if param.requires_grad:
