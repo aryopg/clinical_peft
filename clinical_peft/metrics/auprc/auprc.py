af46985e4b7066456a321f087ed93f1c680b5d0e
Aryo Pradipta Gema <aryopg@gmail.com>
2023-08-13 22:41:40 +0100

    debug

--- clinical_peft/metrics/auprc/auprc.py
+++ clinical_peft/metrics/auprc/auprc.py
@@ -156,7 +156,7 @@ class AUPRC(evaluate.Metric):
     ):
         if self.config_name == "multiclass":
             print(references)
-            labels_onehot = np.zeros((len(references), prediction_scores.shape[-1]))
+            labels_onehot = np.zeros((len(references), len(prediction_scores[0])))
             for i, label in enumerate(references):
                 labels_onehot[i, label] = 1
             references = labels_onehot
