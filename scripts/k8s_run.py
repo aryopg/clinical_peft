616619af458c13f74a2455bc4651341a851bbc57
Aryo Pradipta Gema <aryopg@gmail.com>
2023-10-02 16:45:48 +0100

    create multiple jobs one by one

--- scripts/k8s_run.py
+++ scripts/k8s_run.py
@@ -1,54 +1,61 @@
-from kubejobs.jobs import create_jobs_for_experiments
+from kubejobs.jobs import KubernetesJob
 
-base_command = "git clone 'https://$GIT_TOKEN@github.com/aryopg/clinical_peft.git' --branch longer_sequence && cd clinical_peft && "
+base_command = "git clone 'https://$GIT_TOKEN@github.com/aryopg/clinical_peft.git' --branch longer_sequence && cd clinical_peft && accelerate launch scripts/train.py --config_filepath "
 
 # List of commands to run as separate Kubernetes Jobs
-commands = [
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/llama_7b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/llama_13b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/llama2_7b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/llama2_13b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/pmc_llama_7b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/pmc_llama_13b_lora.yaml",
-    "accelerate launch scripts/train.py --config_filepath configs/mimic_pretrain_hpo_configs/mimic_iv/medllama_13b_lora.yaml",
+configs = [
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/llama_7b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/llama_13b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/llama2_7b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/llama2_13b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/pmc_llama_7b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/pmc_llama_13b_lora.yaml",
+    "configs/mimic_pretrain_hpo_configs/mimic_iv/medllama_13b_lora.yaml",
 ]
 
-commands = [base_command + command for command in commands]
+run_names = [
+    "-".join(config.split("/")[-2:]).replace(".yaml", "").replace("_", "-")
+    for config in configs
+]
+commands = [base_command + config for config in configs]
 
-name = "clinical-llama"
-experiment = "dapt-mimic-iv"
-
-# Create and run Kubernetes Jobs for each command in the list
-create_jobs_for_experiments(
-    commands,
-    image="aryopg/clinical-peft:v1",
-    gpu_type="nvidia.com/gpu",  # Request GPU resources
-    gpu_limit=1,  # Number of GPU resources to allocate
-    backoff_limit=4,  # Maximum number of retries before marking job as failed
-    name=name,
-    experiment=experiment,
-    secret_env_vars={
-        "GIT_TOKEN": {"secret_name": "aryo-git-token", "key": "aryo-git-token"},
-        "WANDB_API_KEY": {
-            "secret_name": "aryo-wandb-api-key",
-            "key": "aryo-wandb-api-key",
-        },
-        "WANDB_PROJECT_NAME": {
-            "secret_name": "aryo-wandb-project-name",
-            "key": "aryo-wandb-project-name",
-        },
-        "WANDB_ENTITY": {
-            "secret_name": "aryo-wandb-entity",
-            "key": "aryo-wandb-entity",
-        },
-        "HF_DOWNLOAD_TOKEN": {
-            "secret_name": "aryo-hf-download-token",
-            "key": "aryo-hf-download-token",
-        },
-        "HF_UPLOAD_TOKEN": {
-            "secret_name": "aryo-hf-upload-token",
-            "key": "aryo-hf-upload-token",
-        },
-        "HF_USERNAME": {"secret_name": "aryo-hf-username", "key": "aryo-hf-username"},
+secret_env_vars = {
+    "GIT_TOKEN": {"secret_name": "aryo-git-token", "key": "aryo-git-token"},
+    "WANDB_API_KEY": {
+        "secret_name": "aryo-wandb-api-key",
+        "key": "aryo-wandb-api-key",
     },
-)
+    "WANDB_PROJECT_NAME": {
+        "secret_name": "aryo-wandb-project-name",
+        "key": "aryo-wandb-project-name",
+    },
+    "WANDB_ENTITY": {
+        "secret_name": "aryo-wandb-entity",
+        "key": "aryo-wandb-entity",
+    },
+    "HF_DOWNLOAD_TOKEN": {
+        "secret_name": "aryo-hf-download-token",
+        "key": "aryo-hf-download-token",
+    },
+    "HF_UPLOAD_TOKEN": {
+        "secret_name": "aryo-hf-upload-token",
+        "key": "aryo-hf-upload-token",
+    },
+    "HF_USERNAME": {"secret_name": "aryo-hf-username", "key": "aryo-hf-username"},
+}
+
+for run_name, command in zip(run_names, commands):
+    # Create a Kubernetes Job with a name, container image, and command
+    job = KubernetesJob(
+        name=run_name,
+        image="aryopg/clinical-peft:v1",
+        gpu_type="nvidia.com/gpu",
+        gpu_limit=1,
+        gpu_product="NVIDIA-A100-SXM4-80GB",
+        backoff_limit=4,
+        command=["/bin/bash", "-c", command],
+        secret_env_vars=secret_env_vars,
+    )
+
+    # Run the Job on the Kubernetes cluster
+    job.run()
