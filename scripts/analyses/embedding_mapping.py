4cc9fb5fceb515c281e14f27c82fac1a436c8678
Aryo Pradipta Gema <aryopg@gmail.com>
2023-11-08 10:45:10 +0000

    fix enumeration bug

--- scripts/analyses/embedding_mapping.py
+++ scripts/analyses/embedding_mapping.py
@@ -43,7 +43,7 @@ def get_text_representation(
     model, inputs, batch_size=4, embedding_pool="last", device=torch.device("cuda:0")
 ):
     embeddings = []
-    for i, name, param in enumerate(model.named_parameters()):
+    for i, (name, param) in enumerate(model.named_parameters()):
         print(i, name, param.data.size())
     model.to(device)
     model.eval()
