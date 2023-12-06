94fd36cef29c6cc21e5df038a62b45a3c9f26adf
Aryo Pradipta Gema <aryopg@gmail.com>
2023-12-06 22:51:32 +0000

    clinical llama baseline experiment

--- scripts/analyses/visualisation_in_domain.py
+++ scripts/analyses/visualisation_in_domain.py
@@ -36,8 +36,10 @@ def argument_parser():
     parser = argparse.ArgumentParser(description="Clinical PEFT")
     parser.add_argument("--llama_embeddings_path", type=str)
     parser.add_argument("--clinical_llama_lora_embeddings_path", type=str)
+    parser.add_argument("--dataset_path", type=str)
     parser.add_argument("--dim_reduction_algo", type=str, default="pca")
     parser.add_argument("--dim", type=int, default=3)
+    parser.add_argument("--specific_icd_code", type=str, default=None)
     parser.add_argument("--random_seed", type=int, default=1234)
     args = parser.parse_args()
     return args
@@ -56,20 +58,34 @@ def dimensionality_reduction(embeddings, algorithm, n_components, random_state):
     return reducer.fit_transform(embeddings)
 
 
-def visualise(embeddings, labels, dim, dim_reduction_algo, model):
+def visualise(
+    embeddings,
+    labels,
+    dim,
+    dim_reduction_algo,
+    model,
+    specific_icd_code=None,
+    texts=None,
+):
     dimensions = [f"Dim{i}" for i in range(1, dim + 1)]
     # Create DataFrames for Plotly
     df_reduced = pd.DataFrame(data=embeddings, columns=dimensions)
-
-    label_mapping = {0: "general", 1: "biomedical", 2: "clinical"}
-    mapped_labels = [label_mapping[label] for label in labels]
-    df_reduced["Labels"] = mapped_labels
+    if specific_icd_code:
+        df_reduced["Labels"] = [
+            1 if specific_icd_code in label.split(",") else 0 for label in labels
+        ]
+        df_reduced["Original labels"] = labels
+    else:
+        df_reduced["Labels"] = labels
 
     if dim == 2:
         fig_reduced = px.scatter(
             df_reduced,
             x="Dim1",
             y="Dim2",
+            # text="Original labels",
+            hover_name="Original labels",
+            # opacity=0,
             color="Labels",
             title=f"{dim_reduction_algo.upper()} Visualization - {model}",
             labels={"Labels": "Labels"},
@@ -81,6 +97,9 @@ def visualise(embeddings, labels, dim, dim_reduction_algo, model):
             x="Dim1",
             y="Dim2",
             z="Dim3",
+            # text="Original labels",
+            hover_name="Original labels",
+            # opacity=0,
             color="Labels",
             title=f"{dim_reduction_algo.upper()} Visualization - {model}",
             labels={"Labels": "Labels"},
@@ -93,34 +112,39 @@ def visualise(embeddings, labels, dim, dim_reduction_algo, model):
 def main() -> None:
     args = argument_parser()
 
+    huggingface_hub.login(token=os.getenv("HF_DOWNLOAD_TOKEN", ""))
+
     common_utils.setup_random_seed(args.random_seed)
+
+    clinical_dataset = load_dataset(args.dataset_path, split="train")
+    print(f"Number of Clinical dataset: {len(clinical_dataset)}")
+
+    num_dataset = len(clinical_dataset)
 
     llama_embeddings = pd.read_csv(args.llama_embeddings_path)
     clinical_llama_lora_embeddings = pd.read_csv(
         args.clinical_llama_lora_embeddings_path
     )
 
-    print(llama_embeddings.head())
-    print(clinical_llama_lora_embeddings.head())
+    llama_labels = llama_embeddings["LABEL"].values[:num_dataset]
+    llama_embeddings = llama_embeddings.iloc[:, :-1].values[:num_dataset]
 
-    llama_labels = llama_embeddings["LABEL"].values
-    llama_embeddings = llama_embeddings.iloc[:, :-1].values
-
-    print(llama_labels)
-    exit()
-
-    clinical_llama_lora_labels = clinical_llama_lora_embeddings["LABEL"].values
-    clinical_llama_lora_embeddings = clinical_llama_lora_embeddings.iloc[:, :-1].values
+    clinical_llama_lora_labels = clinical_llama_lora_embeddings["LABEL"].values[
+        :num_dataset
+    ]
+    clinical_llama_lora_embeddings = clinical_llama_lora_embeddings.iloc[:, :-1].values[
+        :num_dataset
+    ]
 
     assert (llama_labels == clinical_llama_lora_labels).all()
 
-    same_embeddings = 0
-    for llama_embedding, clinical_llama_lora_embedding in zip(
-        llama_embeddings, clinical_llama_lora_embeddings
-    ):
-        if np.array_equal(llama_embedding, clinical_llama_lora_embedding):
-            same_embeddings += 1
-    print(f"{same_embeddings}/{len(llama_embeddings)} embeddings are equal")
+    # same_embeddings = 0
+    # for llama_embedding, clinical_llama_lora_embedding in zip(
+    #     llama_embeddings, clinical_llama_lora_embeddings
+    # ):
+    #     if np.array_equal(llama_embedding, clinical_llama_lora_embedding):
+    #         same_embeddings += 1
+    # print(f"{same_embeddings}/{len(llama_embeddings)} embeddings are equal")
 
     # # Perform PCA on embeddings
     # pca = PCA(n_components=2, random_state=args.random_seed)
@@ -179,6 +203,8 @@ def main() -> None:
         args.dim,
         args.dim_reduction_algo,
         "LLaMA",
+        specific_icd_code=args.specific_icd_code,
+        texts=clinical_dataset["text"],
     )
     visualise(
         clinical_llama_lora_embeddings_reduced,
@@ -186,6 +212,8 @@ def main() -> None:
         args.dim,
         args.dim_reduction_algo,
         "LLaMA + Clinical LLaMA-LoRA",
+        specific_icd_code=args.specific_icd_code,
+        texts=clinical_dataset["text"],
     )
 
 
